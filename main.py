e1ce4e4946f95a27b2ff51c5a24f82cbd704f737
pat <greatsquidkite@gmail.com>
2019-02-27 12:50:44 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+	return render_template("home.html")
+
+@app.route("/samples")
+def samples():
+	return render_template("samples.html")
+
+@app.route("/resume")
+def resume():
+	return render_template("resume.html")
+
+if __name__ == "__main__":
+	app.run(debug=True)
