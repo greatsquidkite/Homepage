dbf5158fc4bbc904a4179d314c0072d5939f88d5
pat <greatsquidkite@gmail.com>
2019-02-28 11:53:19 -0800

    Finished resume and contact pages

--- main.py
+++ main.py
@@ -14,5 +14,9 @@ def samples():
 def resume():
 	return render_template("resume.html")
 
+@app.route("/contact")
+def contact():
+	return render_template("contact.html")
+
 if __name__ == "__main__":
 	app.run(debug=True)
